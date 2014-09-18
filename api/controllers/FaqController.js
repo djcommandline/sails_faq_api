7f06f4348ba8d817a9d6fa462ea8ce06831023aa
wok <wok@foodshelterlove.com>
2014-09-18 10:42:09 -0700

    added faq api - sails generate api faq

---
+++ api/controllers/FaqController.js
@@ -0,0 +1,11 @@
+/**
+ * FaqController
+ *
+ * @description :: Server-side logic for managing faqs
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
