0b606e379dd202f03ec3e4efb96dee1a3a710eac
wok <wok@foodshelterlove.com>
2014-10-02 17:10:57 -0700

    added category property to FAQ model

--- api/models/Faq.js
+++ api/models/Faq.js
@@ -9,6 +9,7 @@ module.exports = {
   attributes: {
     question: { type: 'string' },
     answer: { type: 'string' },
+    category: { type: 'string' },
     joint: { type: 'string' },
     comments: {
       collection: 'message',
