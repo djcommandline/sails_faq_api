50ceb4135244ff237ef7fb973f3d31633bbc0d3f
wok <wok@foodshelterlove.com>
2014-10-03 14:55:31 -0700

    update pipeline to handle more nuance js

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -22,12 +22,19 @@ var cssFilesToInject = [
 // Client-side javascript files to inject in order
 // (uses Grunt-style wildcard/glob/splat expressions)
 var jsFilesToInject = [
-  
+
   // Load sails.io before everything else
   'js/dependencies/sails.io.js',
 
+  // Load Frameworks
+  'js/dependencies/**/*jquery*.js',
+
   // Dependencies like jQuery, or Angular are brought in here
   'js/dependencies/**/*.js',
+
+  // Preload Dependencies we specify from our bower.json
+  // Use $ bower-installer
+  'js/dependencies/**/preload/*.js',
 
   // All of the rest of your client-side js files
   // will be injected here in no particular order.
