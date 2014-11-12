81a9255cffbc84cce09b1b88ca43fa906f0d31aa
wok <wok@foodshelterlove.com>
2014-11-11 18:21:37 -0800

    added promiser api

---
+++ api/controllers/PromiseController.js
@@ -0,0 +1,11 @@
+/**
+ * PromiseController
+ *
+ * @description :: Server-side logic for managing promises
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
