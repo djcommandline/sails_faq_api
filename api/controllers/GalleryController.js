e08aa155b9d26c532aaedb11e44140a8505f363f
wok <wok@foodshelterlove.com>
2014-11-11 18:19:27 -0800

    added gallery

---
+++ api/controllers/GalleryController.js
@@ -0,0 +1,11 @@
+/**
+ * GalleryController
+ *
+ * @description :: Server-side logic for managing galleries
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
